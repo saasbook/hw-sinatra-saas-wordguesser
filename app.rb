5670dfa797279b238eec12dd306e35b708678e7b
iscas-zac <askingyour@126.com>
2021-10-23 21:15:50 +0800

    finish homework

--- app.rb
+++ app.rb
@@ -40,6 +40,15 @@ class WordGuesserApp < Sinatra::Base
   post '/guess' do
     letter = params[:guess].to_s[0]
     ### YOUR CODE HERE ###
+
+    if (!@game.guess(letter))
+        flash[:message] = "You have already used that letter."
+    end
+
+    rescue ArgumentError
+        flash[:message] = "Invalid guess."
+    ensure
+
     redirect '/show'
   end
   
@@ -50,16 +59,32 @@ class WordGuesserApp < Sinatra::Base
   # wrong_guesses and word_with_guesses from @game.
   get '/show' do
     ### YOUR CODE HERE ###
-    erb :show # You may change/remove this line
+
+    state = @game.check_win_or_lose
+    if (state == :play)
+        session[:game] = @game
+        erb :show # You may change/remove this line
+    else if (state == :win)
+	    redirect '/win'
+        else
+            redirect '/lose'
+        end
+    end
   end
   
   get '/win' do
     ### YOUR CODE HERE ###
+
+    session[:game] = @game
+
     erb :win # You may change/remove this line
   end
   
   get '/lose' do
     ### YOUR CODE HERE ###
+
+    session[:game] = @game
+
     erb :lose # You may change/remove this line
   end
   
--- lib/wordguesser_game.rb
+++ lib/wordguesser_game.rb
@@ -6,9 +6,59 @@ class WordGuesserGame
   # Get a word from remote "random word" service
 
   def initialize(word)
-    @word = word
+    @word = word.gsub(' ', '').downcase
+    @guesses = ''
+    @wrong_guesses = ''
   end
 
+  attr_accessor :word
+  attr_accessor :guesses
+  attr_accessor :wrong_guesses
+
+  def guess(guess_letter)
+	  if (!guess_letter or guess_letter.empty? or not ('a' .. 'z') === guess_letter.downcase)
+		  raise ArgumentError
+	  end
+	  guess_letter = guess_letter.downcase
+	  if word.include? guess_letter
+		  if guesses.include? guess_letter
+			  return false
+		  else
+			  @guesses = guesses + guess_letter
+		  end
+	  else
+		  if wrong_guesses.include? guess_letter
+			  return false
+		  else
+			  @wrong_guesses = wrong_guesses + guess_letter
+		  end
+	  end
+	  return true
+  end
+
+  def word_with_guesses()
+	displayed = "-" * @word.length
+	guesses.each_char do | letter |
+		(0..(@word.length - 1)).each do | index |
+			if (@word[index] == letter)
+				displayed[index] = letter
+			end
+		end
+	end
+	return displayed
+  end
+
+  def check_win_or_lose()
+	  if (word_with_guesses.include? "-")
+		  if (wrong_guesses.length >= 7)
+			  return :lose
+		  else
+			  return :play
+		  end
+	  else
+		  return :win
+	  end
+  end
   # You can test it by installing irb via $ gem install irb
   # and then running $ irb -I. -r app.rb
   # And then in the irb: irb(main):001:0> WordGuesserGame.get_random_word

f98861160b3416e1d912a494a6e9258c71c6cbe8
zz <1318781143@qq.con>
2023-12-18 18:02:35 +0800

    feat: implememt the all API in wordguesser
    Implememt route: '/guess', '/win', 'lose'.
    Remove the code in check_win_or_lose about "count=0".

--- app.rb
+++ app.rb
@@ -1,76 +1,88 @@
-require 'sinatra/base'
-require 'sinatra/flash'
-require_relative './lib/wordguesser_game.rb'
+require "sinatra/base"
+require "sinatra/flash"
+require_relative "lib/wordguesser_game"
 
 class WordGuesserApp < Sinatra::Base
-
   enable :sessions
   register Sinatra::Flash
-  
+
   before do
-    @game = session[:game] || WordGuesserGame.new('')
+    @game = session[:game] || WordGuesserGame.new("")
   end
-  
+
   after do
     session[:game] = @game
   end
-  
+
   # These two routes are good examples of Sinatra syntax
   # to help you with the rest of the assignment
-  get '/' do
-    redirect '/new'
+  get "/" do
+    redirect "/new"
   end
-  
-  get '/new' do
+
+  get "/new" do
     erb :new
   end
-  
-  post '/create' do
+
+  post "/create" do
     # NOTE: don't change next line - it's needed by autograder!
     word = params[:word] || WordGuesserGame.get_random_word
     # NOTE: don't change previous line - it's needed by autograder!
 
     @game = WordGuesserGame.new(word)
-    redirect '/show'
+    redirect "/show"
   end
-  
+
   # Use existing methods in WordGuesserGame to process a guess.
   # If a guess is repeated, set flash[:message] to "You have already used that letter."
   # If a guess is invalid, set flash[:message] to "Invalid guess."
-  post '/guess' do
+  post "/guess" do
     letter = params[:guess].to_s[0]
     ### YOUR CODE HERE ###
-    if @game.guesses.include?(letter)
-      flash[:message] = "You have already used that letter."
-    end
-    begin
-      @game.guess(letter)
-    rescue ArgumentError => e
-      flash[:message] = "Invalid guess."
+    if @game.guesses.include?(letter.to_s) || @game.wrong_guesses.include?(letter.to_s)
+      flash[:message] =
+        "You have already used that letter."
     end
 
-    @game.guess(letter)
-    redirect '/show'
+    begin
+      @game.guess(letter)
+    rescue Exception => e
+      flash[:message] = "Invalid guess."
+    end
+    case @game.check_win_or_lose
+    when :win
+      return redirect "/win"
+    when :lose
+      return redirect "/lose"
+    end
+    redirect "/show"
   end
-  
+
   # Everytime a guess is made, we should eventually end up at this route.
   # Use existing methods in WordGuesserGame to check if player has
   # won, lost, or neither, and take the appropriate action.
   # Notice that the show.erb template expects to use the instance variables
   # wrong_guesses and word_with_guesses from @game.
-  get '/show' do
+  get "/show" do
     ### YOUR CODE HERE ###
     erb :show # You may change/remove this line
   end
-  
-  get '/win' do
+
+  get "/win" do
     ### YOUR CODE HERE ###
-    erb :win # You may change/remove this line
+    if @game.check_win_or_lose.eql?(:win)
+      erb :win
+    else
+      redirect "/show"
+    end
   end
-  
-  get '/lose' do
+
+  get "/lose" do
     ### YOUR CODE HERE ###
-    erb :lose # You may change/remove this line
+    if @game.check_win_or_lose.eql?(:lose)
+      erb :lose
+    else
+      redirect "/show"
+    end
   end
-  
 end
--- lib/wordguesser_game.rb
+++ lib/wordguesser_game.rb
@@ -1,80 +1,71 @@
 class WordGuesserGame
-
   # add the necessary class methods, attributes, etc. here
   # to make the tests in spec/wordguesser_game_spec.rb pass.
 
   # Get a word from remote "random word" service
 
-  attr_accessor :word
-  attr_accessor :guesses
-  attr_accessor :wrong_guesses
+  attr_accessor :word, :guesses, :wrong_guesses
 
   def initialize(word)
     @word = word
-    @guesses = ''
-    @wrong_guesses = ''
+    @guesses = ""
+    @wrong_guesses = ""
     @count = 0
   end
 
-  def guess letter
-    raise ArgumentError, 'Invalid guess: empty string' if letter.nil? || letter.empty?
-    raise ArgumentError, 'Invalid guess: not a letter' unless letter.match?(/[A-Za-z]/)
+  def guess(letter)
+    raise ArgumentError, "Invalid guess: empty string" if letter.nil? || letter.empty?
+    raise ArgumentError, "Invalid guess: not a letter" unless letter.match?(/[A-Za-z]/)
+
     letter.downcase!
     if @word.include?(letter)
-      if @guesses.include?(letter)
-        return false
-      else
-        @guesses += letter
-        @count+=1
-        return true
-      end
+      return false if @guesses.include?(letter)
+
+      @guesses += letter
+      @count += 1
+      true
+
     else
-      if wrong_guesses.include?(letter)
-        return false
-      else
-        @wrong_guesses += letter
-        @count+=1
-        return true
-      end
+      return false if wrong_guesses.include?(letter)
+
+      @wrong_guesses += letter
+      @count += 1
+      true
+
     end
   end
 
   def word_with_guesses
-    @word.gsub(/./) do |item| 
-      @guesses.include?(item) ? item: '-'
+    @word.gsub(/./) do |item|
+      @guesses.include?(item) ? item : "-"
     end
   end
 
   def check_win_or_lose
-    
     flag = @word.chars.all? do |c|
       @guesses.include?(c)
     end
 
-    if (@count <= 7 && flag)
+    if @count <= 7 && flag
       @count = 0
-      return :win
-    elsif (@count < 7 && !flag)
-      return :play
+      :win
+    elsif @count < 7 && !flag
+      :play
     else
-      @count = 0
-      return :lose
+      :lose
     end
   end
-
-
 
   # You can test it by installing irb via $ gem install irb
   # and then running $ irb -I. -r app.rb
   # And then in the irb: irb(main):001:0> WordGuesserGame.get_random_word
   #  => "cooking"   <-- some random word
   def self.get_random_word
-    require 'uri'
-    require 'net/http'
-    uri = URI('http://randomword.saasbook.info/RandomWord')
-    Net::HTTP.new('randomword.saasbook.info').start { |http|
+    require "uri"
+    require "net/http"
+    uri = URI("http://randomword.saasbook.info/RandomWord")
+    Net::HTTP.new("randomword.saasbook.info").start do |http|
       return http.post(uri, "").body
-    }
+    end
   end
-
 end

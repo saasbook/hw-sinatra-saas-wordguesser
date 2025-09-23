57c676ac40c697cbdda2bad8411d9587b334d9fa
Matthew Rostkowski <mcr2225@columbia.edu>
2025-09-23 00:17:41 +0000

    WordGuesserGame: all specs passing - Part 1

--- lib/wordguesser_game.rb
+++ lib/wordguesser_game.rb
@@ -3,9 +3,33 @@ class WordGuesserGame
   # to make the tests in spec/wordguesser_game_spec.rb pass.
 
   # Get a word from remote "random word" service
-
+  attr_accessor :word, :guesses, :wrong_guesses
+  # Chp 2.0–2.2
   def initialize(word)
     @word = word
+    @guesses = ''
+    @wrong_guesses = ''
+  end
+  def guess(letter)
+    raise ArgumentError if letter.nil? || letter == '' || letter !~ /\A[a-zA-Z]\z/
+    letter = letter.downcase
+    return false if @guesses.include?(letter) || @wrong_guesses.include?(letter)
+    if @word.downcase.include?(letter)
+      @guesses << letter
+    else 
+      @wrong_guesses << letter
+    end
+    true
+  end
+  def word_with_guesses
+    # Chp 2.3–2.4
+    @word.chars.map { |ch| @guesses.include?(ch.downcase) ? ch : '-' }.join
+  end
+  
+  def check_win_or_lose
+    return :win if word_with_guesses ==@word
+    return :lose if @wrong_guesses.length >= 7
+    :play
   end
 
   # You can test it by installing irb via $ gem install irb
